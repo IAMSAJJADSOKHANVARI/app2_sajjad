151db0b395dcd42e1427bd71fb049c887d0ec437
sajjadsokhanvari <sajjadnetworker2015@gmail.com>
2023-09-13 00:29:01 +0330

    Create columns ,image ,and description

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo2.png",width=600)
+
+with col2:
+    st.title("sajjad sokhanvari")
+    content = """
+    Hi , I am Sajjad! I am a Python programmer,teacher,and founder of PythonHow.
+    
+    
+    """
+    st.info(content)
